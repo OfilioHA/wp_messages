95465408e1c46f8c3af141fa15e41e4cb0bcddd5
Ofilio Herdocia Aráuz <Herdocia20@gmail.com>
2023-08-31 11:48:03 -0600

    Handling static files

--- static/vite.config.js
+++ static/vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      input: {
+        main: './src/main.jsx'
+      },
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
