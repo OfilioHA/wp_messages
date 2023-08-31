701df720aa5444c7ad24bfea48607496af141f8c
OfilioHA <Herdocia20@gmail.com>
2023-08-30 22:37:10 -0600

    Connecting database and models

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask;
-from flask_sqlalchemy import SQLAlchemy;
-
-db = SQLAlchemy();
+from instance.models import db;
 
 app = Flask(__name__);
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
